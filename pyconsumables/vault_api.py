d6090b10625d9473500b82719e6b6d759c606d79
jkdihenkar <jkdihenkar@gmail.com>
2017-06-05 14:56:27 +0530

    major refactoring of config to make it override-able

--- pyconsumables/vault_api.py
+++ pyconsumables/vault_api.py
@@ -14,27 +14,28 @@ class VaultAPI(object):
     A little higher level vault API
     """
     def __init__(self):
+        self.vault_conf = VaultConfig()
         self.vault_cli = hvac.Client(
-            url=VAULT_URL,
-            token=VAULT_TOKEN_ROOT,
-            verify=VAULT_VERIFY,
-            timeout=VAULT_TIMEOUT,
-            allow_redirects=VAULT_ALLOW_REDIRECTS
+            url=self.vault_conf.vault_url,
+            token=self.vault_conf.vault_token_root,
+            verify=self.vault_conf.vault_verify,
+            timeout=self.vault_conf.vault_timeout,
+            allow_redirects=self.vault_conf.vault_allow_redirects
         )
 
     def get_seal_status(self) -> bool:
         return self.vault_cli.is_sealed()
 
     def attempt_unseal_old(self):
-        self.vault_cli.unseal(key=VAULT_UNSEAL_KEYS[0])
-        self.vault_cli.unseal(key=VAULT_UNSEAL_KEYS[1])
-        self.vault_cli.unseal(key=VAULT_UNSEAL_KEYS[2])
+        self.vault_cli.unseal(key=self.vault_conf.vault_unseal_keys[0])
+        self.vault_cli.unseal(key=self.vault_conf.vault_unseal_keys[1])
+        self.vault_cli.unseal(key=self.vault_conf.vault_unseal_keys[2])
 
     async def async_attempt_unseal(self):
-        return await self.vault_cli.unseal_multi(VAULT_UNSEAL_KEYS)
+        return await self.vault_cli.unseal_multi(self.vault_conf.vault_unseal_keys)
 
     def attempt_unseal(self):
-        return self.vault_cli.unseal_multi(VAULT_UNSEAL_KEYS)
+        return self.vault_cli.unseal_multi(self.vault_conf.vault_unseal_keys)
 
     def write_secret(self, path: str, warp_ttl: int, kwargs: dict) -> dict:
         return self.vault_cli.write(
@@ -54,12 +55,12 @@ class VaultAPI(object):
         Write the keys to a dump file
         """
         out = self.vault_cli.start_rekey(
-            **VAULT_KEY_PROPS
+            **self.vault_conf.vault_key_props
         )
 
-        dumpfile = VAULT_REKEYING_DUMP_FILE.format(date_time=datetime.datetime.now().strftime("%Y%m%d_%H%M%S"))
+        dumpfile = self.vault_conf.vault_rekeying_dump_file.format(date_time=datetime.datetime.now().strftime("%Y%m%d_%H%M%S"))
 
-        keys_out = self.vault_cli.rekey_multi(VAULT_UNSEAL_KEYS, out['nonce'])
+        keys_out = self.vault_cli.rekey_multi(self.vault_conf.vault_unseal_keys, out['nonce'])
 
         with open(dumpfile, 'w+') as dumpfile_fd:
             dumpfile_fd.write(json.dumps(keys_out))
@@ -80,14 +81,14 @@ class VaultAPI(object):
         Init approle backend
         STATELESS - can be called everytime without side effects
         """
-        if VAULT_APPROLE_BACKEND_ENABLED and not self.check_auth_backend_mounted(VAULT_APPROLE_BACKEND_CHECK):
+        if self.vault_conf.vault_approle_backend_enabled and not self.check_auth_backend_mounted(self.vault_conf.vault_approle_backend_check):
             self._enable_auth_role()
             return {'mounted': True, 'message': "Mount success" }
         else:
             return {'mounted': True, 'message': 'NoAction'}
 
     def _enable_auth_role(self):
-        return self.vault_cli.enable_auth_backend(**VAULT_APPROLE_BACKEND)
+        return self.vault_cli.enable_auth_backend(**self.vault_conf.vault_approle_backend)
 
     def _unmount_approle_backend(self, do_you_understand_what_it_does=False):
         """
@@ -105,7 +106,7 @@ class VaultAPI(object):
         else:
             return False
 
-    def _enable_audit_file_backend(self, file=VAULT_AUDIT_LOG_FILE):
+    def _enable_audit_file_backend(self, file=''):
         return self.vault_cli.enable_audit_backend(
             'file',
             'Vault logs to file',
--- pyconsumables/vault_confs.py
+++ pyconsumables/vault_confs.py
@@ -6,45 +6,50 @@ String vault confs
 #  and a string to CA bundle of we need to give a specific CA.
 #  sessions.py => line #479
 
-# Connect to vault
-VAULT_URL = "https://localhost:9801"
-VAULT_VERIFY = '/home/jd/hashicorp/ca.pem'
-VAULT_TIMEOUT = 30
-VAULT_ALLOW_REDIRECTS = True
+# Python namespaces - Use more of it.
 
-# Root Token
-VAULT_TOKEN_ROOT = "0891a60b-a132-b149-2ea2-2562316909b8"
 
-# Re-key params
-VAULT_KEY_PROPS = {
-    'secret_shares': 5,
-    'secret_threshold': 2,
-    'pgp_keys': None,
-    'backup': True
-}
+class VaultConfig(object):
 
-# If you re-key vault you'll need to update this value
-VAULT_UNSEAL_KEYS = [
-    "64N8/B4b7TQlXaJnWBlKkeDqwDBojeoohsm5wQGwvhAy",
-    "EcVseyVYlCHbFXBrml3ODQmwlRz4x8ImYuCkMXOiVK4z",
-    "+WeW5/MjUwKArQqlGivsqKUmcz2Tq1A8ahj6cHeMRjBZ",
-    "lqB0Mo63XLNaecthOTsI3xnHZyYgqP4v9FA6uTi5y0+/",
-    "12+E3uxLzXAn7AklC+r1qrm8SZkRI311H9yBnsBXet+w"
-]
+    def __init__(self):
+        # Connect to vault
+        self.vault_url = "https://localhost:9801"
+        self.vault_verify = '/home/jd/hashicorp/ca.pem'
+        self.vault_timeout = 30
+        self.vault_allow_redirects = True
 
-# Vault dump path after rekeying
-VAULT_REKEYING_DUMP_FILE = '/home/jd/hashicorp/pyconsumables/rekey_{date_time}.json'
+        # Root Token
+        self.vault_token_root = "0891a60b-a132-b149-2ea2-2562316909b8"
 
-# Approle Backend Details
-VAULT_APPROLE_BACKEND_ENABLED = True
-VAULT_APPROLE_BACKEND_CHECK = 'auth/approle/'
-VAULT_APPROLE_BACKEND = {
-    'backend_type': 'approle',
-    'description': 'Applications and Role Descriptions',
-    'mount_point': '/auth/approle'
-}
+        # Re-key params
+        self.vault_key_props = {
+            'secret_shares': 5,
+            'secret_threshold': 2,
+            'pgp_keys': None,
+            'backup': True
+        }
 
-# Audit Backend
-VAULT_AUDIT_LOG_FILE = '/home/jd/hashicorp/vault.m.log'
+        # If you re-key vault you'll need to update this value
+        self.vault_unseal_keys = [
+            "64N8/B4b7TQlXaJnWBlKkeDqwDBojeoohsm5wQGwvhAy",
+            "EcVseyVYlCHbFXBrml3ODQmwlRz4x8ImYuCkMXOiVK4z",
+            "+WeW5/MjUwKArQqlGivsqKUmcz2Tq1A8ahj6cHeMRjBZ",
+            "lqB0Mo63XLNaecthOTsI3xnHZyYgqP4v9FA6uTi5y0+/",
+            "12+E3uxLzXAn7AklC+r1qrm8SZkRI311H9yBnsBXet+w"
+        ]
 
+        # Vault dump path after rekeying
+        self.vault_rekeying_dump_file = '/home/jd/hashicorp/pyconsumables/rekey_{date_time}.json'
+
+        # Approle Backend Details
+        self.vault_approle_backend_enabled = True
+        self.vault_approle_backend_check = 'auth/approle/'
+        self.vault_approle_backend = {
+            'backend_type': 'approle',
+            'description': 'Applications and Role Descriptions',
+            'mount_point': '/auth/approle'
+        }
+
+        # Audit Backend
+        self.vault_audit_log_file = '/home/jd/hashicorp/vault.m.log'
 
