a89e9a98238572566b04eda9ccb92410b331bf01
jkdihenkar <jkdihenkar@gmail.com>
2017-06-05 16:28:59 +0530

    actually building something now

---
+++ pyconsumables/vault-api/__init__.py
@@ -0,0 +1,9 @@
+# Author : JayD ( jay.dihenkar@evivehealth.com )
+# Version : v0.1
+# Date : 
+# Aim [ Redmine Ticket ] : 
+# Copyright (c) 2017.
+# This  code file/snippet/block, including any other configuration files,
+#   is for the sole use of the Evive Health, LLC and may contain business
+#   confidential and privileged information.
+# Any unauthorized review, use, disclosure or distribution is prohibited.
